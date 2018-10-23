38e4ececbc26a054f0fdbaa4779ea8d3b061618c
Christian Tomoscheit <christian.tomoscheit@gmail.com>
2018-10-23 08:28:55 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.clojars.ctomo/lein-xjc "0.2.2"
+(defproject org.clojars.ctomo/lein-xjc "0.2.3"
   :description "Call xjc from leiningen."
   :url "https://github.com/ctomo/lein-xjc"
   :scm {:name "git"
         :url "https://github.com/ctomo/lein-xjc"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.glassfish.jaxb/jaxb-xjc "2.3.0"]
-                 [org.glassfish.jaxb/jaxb-runtime "2.3.0"]
+  :dependencies [[org.glassfish.jaxb/jaxb-xjc "2.3.1"]
+                 [org.glassfish.jaxb/jaxb-runtime "2.3.1"]
                  [javax.activation/javax.activation-api "1.2.0"]]
   :eval-in-leiningen true
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.6.3"]
+                   :dependencies [[midje "1.9.4"]
                                   [cljito "0.2.1"]
-                                  [org.mockito/mockito-all "1.9.5"]]
+                                  [org.mockito/mockito-all "1.10.19"]]
                    :plugins [[lein-midje "3.1.3"]]}})
